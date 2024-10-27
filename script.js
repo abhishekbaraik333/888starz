e99da831d7312d2f1a63e384de97791dcf1e4de1
abhishekbaraik333 <abhitamangme@gmail.com>
2024-10-27 21:36:27 +0530

    language added

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const mobileNav = document.getElementById("mobile-nav");
-    const close = document.getElementById("close");
-  
-    hamburger.addEventListener("click", () => {
-      mobileNav.classList.toggle("active");
-      hamburger.classList.toggle('active');
-  
-    });
-    close.addEventListener("click", () => {
-      mobileNav.classList.toggle("active");
-    });
-}
-)  
+  const hamburger = document.getElementById("hamburger");
+  const mobileNav = document.getElementById("mobile-nav");
+
+  hamburger.addEventListener("click", () => {
+    mobileNav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
+});
